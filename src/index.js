8f36261a39777ed659ae2cc5a0cd891dba60505c
github username <github email address>
2020-05-12 13:08:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App'
+import ParentComponent from './components/ParentComponent'
 
 ReactDOM.render(
-  <App />,
+  <ParentComponent />,
   document.getElementById("root")
 );
